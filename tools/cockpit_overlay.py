3282c80cb21f9a56610a6be624c1e502a0e3a29f
JackDesBwa <jackdesbwa@gmail.com>
2023-03-23 23:09:52 +0100

    Add (too naive) IMU 3D representation in cockpit

--- tools/cockpit_overlay.py
+++ tools/cockpit_overlay.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-from math import sin, cos, pi
+from math import pi, atan2
+from pyglet.math import Mat4, Vec3, Vec4
 
 from cardetector import CarDetector
 from carmessagesrx import CarMulticastReceiver, CarMulticastDecoder
@@ -16,7 +17,7 @@ if __name__ == '__main__':
     car_info = detector.choose_car()
     if not car_info:
         print('No car.')
-        exit()
+        car_info = {'ip': '', 'name': 'CarNode-NoCar'}
     car_ip = car_info['ip']
 
     import pyglet
@@ -36,23 +37,35 @@ if __name__ == '__main__':
     img_throttle_mask.anchor_y = 145/2
     window.set_icon(img_steering)
 
-    batch = pyglet.graphics.Batch()
+    batch2D = pyglet.graphics.Batch()
     grp1 = pyglet.graphics.Group(1)
-    board = pyglet.sprite.Sprite(img=img_board, x=0, y=0, batch=batch)
-    name_txt = pyglet.text.Label(car_info['name'][8:], x=window.width//2, y=80, font_size=25, color=(210, 210, 210, 255), anchor_x='center', batch=batch)
-    steering = pyglet.sprite.Sprite(img=img_steering, x=window.width//2, y=14, batch=batch, group=grp1)
-    rssi_bar = pyglet.shapes.Rectangle(x=20, y=12, width=80, height=8, color=(0x00, 0x88, 0xAA), batch=batch)
-    batt_bar = pyglet.shapes.Rectangle(x=1180, y=12, width=80, height=8, color=(0x00, 0x88, 0xAA), batch=batch)
-    batt_txt = pyglet.text.Label('--', x=1170, y=12, font_size=20, color=(30, 30, 30, 255), anchor_x='right', batch=batch)
-    rssi_txt = pyglet.text.Label('--', x=110, y=12, font_size=20, color=(30, 30, 30, 255), anchor_x='left', batch=batch)
-    throttle = pyglet.shapes.Sector(x=375, y=85, radius=70, angle=0, start_angle=-3*pi/4, color=(0, 0, 0, 255), batch=batch)
-    throttle_mask = pyglet.sprite.Sprite(img=img_throttle_mask, x=375, y=85, batch=batch, group=grp1)
-    thr_txt = pyglet.text.Label('--', x=375, y=78, font_size=18, color=(30, 30, 30, 255), anchor_x='center', batch=batch)
+    board = pyglet.sprite.Sprite(img=img_board, x=0, y=0, batch=batch2D)
+    name_txt = pyglet.text.Label(car_info['name'][8:], x=window.width//2, y=80, font_size=25, color=(210, 210, 210, 255), anchor_x='center', batch=batch2D)
+    steering = pyglet.sprite.Sprite(img=img_steering, x=window.width//2, y=14, batch=batch2D, group=grp1)
+    rssi_bar = pyglet.shapes.Rectangle(x=20, y=12, width=80, height=8, color=(0x00, 0x88, 0xAA), batch=batch2D)
+    batt_bar = pyglet.shapes.Rectangle(x=1180, y=12, width=80, height=8, color=(0x00, 0x88, 0xAA), batch=batch2D)
+    batt_txt = pyglet.text.Label('--', x=1170, y=12, font_size=20, color=(30, 30, 30, 255), anchor_x='right', batch=batch2D)
+    rssi_txt = pyglet.text.Label('--', x=110, y=12, font_size=20, color=(30, 30, 30, 255), anchor_x='left', batch=batch2D)
+    throttle = pyglet.shapes.Sector(x=375, y=85, radius=70, angle=0, start_angle=-3*pi/4, color=(0, 0, 0, 255), batch=batch2D)
+    throttle_mask = pyglet.sprite.Sprite(img=img_throttle_mask, x=375, y=85, batch=batch2D, group=grp1)
+    thr_txt = pyglet.text.Label('--', x=375, y=78, font_size=18, color=(30, 30, 30, 255), anchor_x='center', batch=batch2D)
+    batch3D= pyglet.graphics.Batch()
+    diode = pyglet.resource.model("diode.obj", batch=batch3D)
+    diode.matrix = Mat4.from_rotation(pi/2, (1, 0, 0)) @ Mat4.from_translation((0, 0, -10))
 
     @window.event
     def on_draw():
         window.clear()
-        batch.draw()
+
+        pyglet.gl.glDisable(pyglet.gl.GL_DEPTH_TEST)
+        window.projection = Mat4.orthogonal_projection(0, window.width, 0, window.height, -1, 1)
+        window.viewport = (0, 0, window.width, window.height)
+        batch2D.draw()
+
+        pyglet.gl.glEnable(pyglet.gl.GL_DEPTH_TEST)
+        window.projection = Mat4.perspective_projection(window.aspect_ratio, z_near=0.1, z_far=255)
+        window.viewport = (178, 5, window.width+178, window.height+5)
+        batch3D.draw()
 
     def set_rssi(v):
         p = min(max(0, v + 120) / 120, 1)
@@ -82,6 +95,13 @@ if __name__ == '__main__':
             throttle.color = (0xAA, 0x00, 0x00)
         thr_txt.text = str(round(p*100)) + '%'
 
+    def set_imu(v):
+        a = Vec3(-v['imu_a_y'], v['imu_a_z'], -v['imu_a_x']).normalize()
+        rot_x = Mat4.from_rotation(pi/4-atan2(a[2], a[1]), Vec3(1, 0, 0))
+        rot_y = Mat4.from_rotation(atan2(a[0], a[1]), Vec3(*(rot_x @ Vec4(0, 0, 1, 0))[:3]))
+        trans = Mat4.from_translation(Vec3(0, 0, -10))
+        diode.matrix = rot_x @ rot_y @ trans
+
     def handle_msgs(dt):
         packet, sender = receiver.recv()
         if sender[0] == car_ip:
@@ -94,6 +114,8 @@ if __name__ == '__main__':
                     set_batt(m['batt_adc'])
                 if 'RSSI' in m:
                     set_rssi(m['RSSI'])
+                if 'imu_a_x' in m:
+                    set_imu(m)
 
     pyglet.clock.schedule_interval(handle_msgs, 0.005)
     pyglet.app.run()
